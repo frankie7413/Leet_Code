9c13ab613f9afce622235e974639137223272893
frankie7413 <frankie7413@gmail.com>
2023-01-16 14:54:57 -0800

    new example

---
+++ examples/counter_coding.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Record
+end
+
+# AC
+# Count events inserted 
+# clear out events that are older than 5 minutes 
+# events return events counted for the past 5 minutes 
+
+record = Record.new
+record.insert('e1')
+record.insert('e1')
+record.insert('e2')
+
+# { e1: 2, e2: 1 }
+puts record.events
+
+sleep(10)
+
+# { }
+puts record.events
+
+record.insert('e1')
+
+# { e1: 1 }
+puts record.events
