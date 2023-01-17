dbb59e78861f6aea5c2f5edf325ff400bf4e1171
frankie7413 <frankie7413@gmail.com>
2023-01-16 16:14:09 -0800

    solution for example

--- examples/counter_coding.rb
+++ examples/counter_coding.rb
@@ -1,14 +1,36 @@
 # frozen_string_literal: true
 
-# counter tracking class
+# event counter class
 class Record
-  def intialize; end
+  def initialize
+    @events = {}
+  end
+
+  def insert(value)
+    if @events.key?(value)
+      @events[value][:count] += 1
+      return
+    end
+
+    @events[value] = { count: 1, time: Time.now }
+  end
+
+  def clear_cache
+    return if @events.empty?
+
+    @events.select! { |_k, value| Time.now - value[:time] < 5 }
+  end
+
+  def events
+    clear_cache
+    @events.transform_values { |v| v[:count] }
+  end
 end
 
 # AC
 # Count events inserted 
-# clear out events that are older than 5 minutes 
-# events return events counted for the past 5 minutes 
+# clear out events that are older than 5 seconds
+# events return events counted for the past 5 seconds
 
 record = Record.new
 record.insert('e1')
@@ -18,6 +40,7 @@ record.insert('e2')
 # { e1: 2, e2: 1 }
 puts record.events
 
+puts 'sleeping...'
 sleep(10)
 
 # { }
